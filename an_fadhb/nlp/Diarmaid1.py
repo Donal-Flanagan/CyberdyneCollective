bdb6105345ccb0ed1032061e1ae092a5a8ea4074
Diarmaid de Burca <diarmaiddeburca+prog@gmail.com>
2017-05-13 18:03:04 +0100

    Now will return a list of the size passed in, where the elements are the 'important' words

--- an_fadhb/nlp/Diarmaid1.py
+++ an_fadhb/nlp/Diarmaid1.py
@@ -1,38 +1,80 @@
-
 import nltk
 import numpy
 from nltk import word_tokenize
+import pandas as pd
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from collections import Counter
+from operator import itemgetter
 
+nltk.download("stopwords")
 
-def create_lexicon(fileIn):
-    nltk.download("stopwords")
-    nltk.download("punkt")
-    stop_words = set(stopwords.words("english"))
-
+def get_all_words_in_order(fileIn):
+    #We want to lemmatize words so that plurals etc. are counted as the same word
     lemmitizer = WordNetLemmatizer()
+    # We are not interested in stop words
+    stop_words = set(stopwords.words("english"))
     lexicon=[]
-    tokenizer = RegexpTokenizer(r'\w+')
     with open(fileIn, 'r') as f:
         contents = f.readlines()
         for l in contents[:]:
             all_words = word_tokenize(l)
             for i in all_words:
                 if i.lower() not in stop_words:
+                    #We don't want numbers or punctuation
                     if i.isalnum():
-                        lexicon.append(i)
+                        lexicon.append(i.lower())
 
     lexicon = [lemmitizer.lemmatize(i) for i in lexicon]
-    print(lexicon)
-    w_counts = Counter(lexicon)
-    l2 =[]
+    return lexicon
+
+def get_most_important_words(fileIn,n):
+    #Get all key words
+    key_words = get_all_words_in_order(fileIn)
+    key_words = key_words +create_all_key_words(key_words)
+
+
+    newlist = sort_key_words(key_words)
+
+    return newlist[0:n]
+
+def sort_key_words(listIn):
+    w_counts = Counter(listIn)
+    l2 = []
     for w in w_counts:
-        l2.append([w, w_counts[w]])
+        if (w.count(' ') == 0):
+            mult = 1
+        else:
+            mult = w.count(' ') + 1
+
+        l2.append([w, w_counts[w] * mult])
+
+    l3 = sorted(l2, key=itemgetter(1), reverse=True)
+
+    newlist = [x[:1][0] for x in l3]
+    return newlist
+
+#I assumed that key words would follow each other - didn't consider the scenario where key words had words between them
+def create_all_key_words(lexiconIn):
+    lexicon2 = []
+    for i in range(1,len(lexiconIn)-2):
+        doubleTest = lexiconIn[i]+" " +lexiconIn[i+1]
+        toTest = doubleTest +" " +lexiconIn[i+2]
+        for j in range(1,len(lexiconIn)-2):
+            testAgainstDouble = lexiconIn[j] +" "+ lexiconIn[j+1]
+            testAgainst = testAgainstDouble+" "+ lexiconIn[j+2]
+            if (toTest == testAgainst):
+                lexicon2.append(toTest)
+            if (testAgainstDouble == doubleTest):
+                lexicon2.append(doubleTest)
+
+    return lexicon2
+
+
+
 
 
 if __name__ == "__main__":
-    print("What I want to print")
-    create_lexicon("script1.txt")
+    list = get_most_important_words("script.txt",5)
+    print (list)
