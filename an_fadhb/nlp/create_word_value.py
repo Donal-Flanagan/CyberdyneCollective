5d6364c7daedfb19a516a6d0b52d57a1ddedb43d
Diarmaid de Burca <diarmaiddeburca+prog@gmail.com>
2017-05-14 03:03:28 +0100

    Added some stuff, changed how we calculate words

--- an_fadhb/nlp/create_word_value.py
+++ an_fadhb/nlp/create_word_value.py
@@ -19,20 +19,33 @@ nltk.download("wordnet")
 
 
 
-def assign_value(words):
+def get_keywords_and_values(words):
     """
     This is where you rank the words. 
     """
     d={}
-    w_counts = Counter(words)
-    for w in w_counts:
-        if w.count(' ') == 0:
-            mult = 1.0
-        else:
-            mult = w.count(' ') + 1.0
+    triple_keyword_value = 5
+    double_keyword_value= 3
+    single_keyword_occurance_value = 1
 
-        d[w]=w_counts[w]*mult
+    stop_words = set(stopwords.words("english"))
 
+    for i in range(0, len(words)-2):
+        if words[i] not in stop_words and words[i].isalnum():
+            d[words[i]] = d.get(words[i],0.0)+ single_keyword_occurance_value
+            if words[i+1] not in stop_words and words[i+1].isalnum():
+                d[words[i]+" "+words[i+1]] = d.get(words[i]+" "+words[i+1],0.0)+double_keyword_value
+                if words[i + 2] not in stop_words and words[i + 2].isalnum():
+                    d[words[i]+" "+words[i+1]+" "+words[i+2]] = d.get(words[i]+" "+words[i+1]+" "+words[i+2],0.0)+triple_keyword_value
+
+    print(i, len(words))
+
+    if words[i+1] not in stop_words and words[i+1].isalnum():
+        d[words[i+1]] = d.get(words[i+1],0.0)+ single_keyword_occurance_value
+        if words[i+2] not in stop_words and words[i+2].isalnum():
+             d[words[i+1]+" "+words[i+2]] = d.get(words[i+1]+" "+words[i+2],0.0)+double_keyword_value
+    if words[i+2] not in stop_words and words[+2].isalnum():
+        d[words[i+2]] = d.get(words[i+2],0.0)+ single_keyword_occurance_value
     return d
 
 
@@ -40,40 +53,19 @@ def get_lemitized_words_in_order(file_in):
     # We want to lemmatize words so that plurals etc. are counted as the same word
     lemmitizer = WordNetLemmatizer()
     # We are not interested in stop words
-    stop_words = set(stopwords.words("english"))
+
     lexicon=[]
+    print(file_in)
     with open(file_in, 'r') as f:
         contents = f.readlines()
         for l in contents[:]:
             all_words = word_tokenize(l)
             for i in all_words:
-                if i.lower() not in stop_words:
-                    # We don't want numbers or punctuation
-                    if i.isalnum():
-                        lexicon.append(i.lower())
-
+                lexicon.append(i.lower())
     lexicon = [lemmitizer.lemmatize(i) for i in lexicon]
     return lexicon
 
 
-def create_all_key_words(file_in):
-    """
-    This is where I create all of the key words.   I assume that keywords will appear in the text in order,
-    and the maximum length of the key words is 3. Stop words can appear between keywords.
-    :param file_in:
-    :return:
-    """
-    lexicon2 = []
-
-    key_words = get_lemitized_words_in_order(file_in)
-
-    for i in range(1, len(key_words)-2):
-        double_keyword = key_words[i] + " " + key_words[i + 1]
-        triple_keyword = double_keyword + " " + key_words[i + 2]
-        key_words.append(double_keyword)
-        key_words.append(triple_keyword)
-
-    return key_words
 
 
 def create_words_and_values(file_in):
@@ -82,27 +74,30 @@ def create_words_and_values(file_in):
     file_in is the path to the file, and n is the number of words to get
     """
     # Get all key words
-    key_words = create_all_key_words(file_in)
+    print(file_in,"CR")
+    words_in_order = get_lemitized_words_in_order(file_in)
 
-    words_and_values = assign_value(key_words)
+    words_and_values = get_keywords_and_values(words_in_order)
 
     return words_and_values
 
+def save_words_and_values(file_in, save_file):
+
+    results = create_words_and_values(file_in)
+    print(results)
+
+    pickle.dump(results, open(save_file, 'wb'))
 
 def main():
     # number = eval(input("How many words do you want to get?"))
 
-    file_in = input("Please input the file to create the list from")
-    #file_in = "script1.txt"
-    save_file = input("Please input the save file name")
-    #save_file = "test.pickle"
+    #file_in = input("Please input the file to create the list from")
+    file_in = "script1.txt"
+    #save_file = input("Please input the save file name")
+    save_file = "test.pickle"
     print("Calculating values from" , file_in, " and saving list to ", save_file)
 
-    results = create_words_and_values(file_in)
-    print(results)
-    print(save_file)
-
-    pickle.dump(results, open(save_file, 'wb'))
+    save_words_and_values(file_in,save_file)
 
 if __name__ == "__main__":
     main()
