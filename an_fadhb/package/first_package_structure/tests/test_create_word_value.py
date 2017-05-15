14b08e5429d155a8b0f6ea549c9cb7f0a8bfcb3b
Donal-Flanagan <d.flanagan@fredknows.it>
2017-05-15 13:15:22 +0200

    The main package, added for tech review

---
+++ an_fadhb/package/first_package_structure/tests/test_create_word_value.py
@@ -0,0 +1,70 @@
+import os
+import unittest
+
+from first_package_structure import create_word_value as cwv
+
+
+class TestTus(unittest.TestCase):
+    fileName="testfile"
+
+    def write_file(self,list):
+        string_to_write=""
+
+        for i in range(0,len(list)):
+            string_to_write=string_to_write+list[i]+" "
+
+        file = open(self.fileName, 'w')
+
+        file.write(string_to_write)
+
+        file.close()
+
+    def remove_file(self):
+        os.remove(self.fileName)
+
+
+    def test_get_all_words_in_order(self):
+        list = ['one','two','three']
+        expected = ['one','two','three']
+        self.write_file(list)
+
+
+        gotten = cwv.get_lemitized_words_in_order('testFile')
+
+        self.remove_file()
+
+        self.assertEquals(expected, gotten)
+
+    def test_get_key_words_and_values(self):
+        input = ['one','two','three']
+
+        expected = {}
+        expected['one']=1.0
+        expected['two']=1.0
+        expected['three']=1.0
+        expected['one two'] =3.0
+        expected['two three']= 3.0
+        expected['one two three']=5.0
+        gotten = cwv.get_keywords_and_values(input)
+
+        self.assertEquals(expected, gotten)
+
+    def test_create_words_and_values(self):
+        input = ['one','two','three']
+
+        self.write_file(input)
+        expected = {}
+        expected['one']=1.0
+        expected['two']=1.0
+        expected['three']=1.0
+        expected['one two'] =3.0
+        expected['two three']= 3.0
+        expected['one two three']=5.0
+        gotten = cwv.create_words_and_values(self.fileName)
+        self.remove_file()
+
+        self.assertEquals(expected, gotten)
+
+
+
+
