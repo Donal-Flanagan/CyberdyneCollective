02126f0bd9b5c8b09a0dd3578c0d070400a55497
Markus Krecik <markus.krecik@gmail.com>
2017-07-17 10:40:30 +0200

    distributions experimentation

--- test_datasets/utils.py
+++ test_datasets/utils.py
@@ -10,15 +10,33 @@ y_column = 'target'
 
 drop_dict = {}
 replace_dict = {}
-convert_dict = {'target': pd.to_datetime,
-                     'planned_installation_time': pd.to_numeric,
-                     'status_type': pd.to_numeric}
 
 def get_era(df):
     return df['era'].apply(lambda s: int(s[3:]))
 
 feature_dict = {'era_int': get_era}
+convert_dict = {'target': lambda x: x.astype('category'),
+                'era': lambda x: x.astype('category')}
 
+model_params = {'base_score': 0.5,
+                'colsample_bylevel': 1,
+                'colsample_bytree': 1,
+                'gamma': 0,
+                'learning_rate': 0.1,
+                'max_delta_step': 0,
+                'max_depth': 2,
+                'min_child_weight': 1,
+                'missing': None,
+                'n_estimators': 500,
+                'nthread': -1,
+                'objective': 'binary:logistic',
+                'reg_alpha': 0,
+                'reg_lambda': 1,
+                'scale_pos_weight': 1,
+                'seed': 0,
+                'silent': True,
+                'subsample': 0.5 #1
+                }
 
 
 def dump(obj, file):
