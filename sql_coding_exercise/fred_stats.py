5579d9318f148cbf1546fb988f43e31c91accc33
Donal-Flanagan <d.flanagan@fredknows.it>
2017-09-24 13:19:48 +0200

    Dates are now passed as years and the table_name is now an additional option

--- sql_coding_exercise/fred_stats.py
+++ sql_coding_exercise/fred_stats.py
@@ -4,7 +4,7 @@
 Retrieve statistics from the FRED api
 
 Usage:
-    fred_stats.py --user=USER --password=PASSWORD [--start=START] [--end=END] [--host=HOST] [--api_key=API_KEY] [--database=DATABASE] [--port=PORT]
+    fred_stats.py --user=USER --password=PASSWORD [--start=START] [--end=END] [--database=DATABASE] [--table=TABLE] [--host=HOST] [--api_key=API_KEY] [--port=PORT]
     fred_stats.py (-h | --help)
     fred_stats.py (-v | --version)
 
@@ -19,13 +19,15 @@ Additional options:
                                 Default = 1980
     -e --end=END                yyyy. End year for average unemployment rate aggregation.
                                 Default = 2015
-    --host=HOST                 Database hostname.
-                                Default = 'localhost'.
-    -a --api_key=API_KEY        Your FRED api key.
-                                Default = Fred API key of Dónal Flanagan.
     -d --database=DATABASE      The name of the database in which you wish to store the Fred data.
                                 If the database does not exist it will be created.
                                 Default = 'fred_db'.
+    -t --table=TABLE            The name of the table in which you wish to insert/update the data.
+                                Default = 'fred_data
+    -a --api_key=API_KEY        Your FRED api key.
+                                Default = Fred API key of Dónal Flanagan.
+    --host=HOST                 Database hostname.
+                                Default = 'localhost'.
     --port=PORT                 The Postgres port.
                                 Default = 5432
 """
@@ -56,7 +58,7 @@ def get_db_engine(db_name, user, password, host, port=5432):
     engine = create_engine(db_connection)
     if not database_exists(engine.url):
         try:
-            logger('database created: %s', db_name)
+            logger('Database created: %s', db_name)
             create_database(engine.url)
 
         except Exception as exc:
@@ -73,12 +75,17 @@ def main():
     host = args.get('--host') if args.get('--host') else 'localhost'
     api_key = args.get('--api_key') if args.get('--api_key') else '01af77900eb060649a7c504ee0705b4d'
     db_name = args.get('--database') if args.get('--database') else 'fred_db'
+    table_name = args.get('--table') if args.get('--table') else 'fred_data'
     port = int(args.get('--port')) if args.get('--port') else 5432
     start_year = args.get('--start') if args.get('--start') else '1980'
     end_year = args.get('--end') if args.get('--end') else '2015'
 
     start_date = '{start_year}-01-01'.format(start_year=start_year)
     end_date = '{end_year}-12-31'.format(end_year=end_year)
+
+    print('\n--------------------------------')
+    print('start_date:', start_date)
+    print('end_date:', end_date)
 
     # Instantiate the sqlalchemy database interface
     engine = get_db_engine(db_name, user, password, host, port)
@@ -97,8 +104,8 @@ def main():
         fred_data = pd.concat(frames, axis=1)
 
         # Insert the data into an SQL table
-        # fixme: add option to replace or append here depending on initial or incremental load type
-        fred_data.to_sql(name='fred_data', con=engine, if_exists='replace', index=True, index_label='timestamp')
+        # fixme: add option to replace or append here depending on initial or incremental load type - update replace
+        fred_data.to_sql(name=table_name, con=engine, if_exists='replace', index=True, index_label='timestamp')
 
         # Query the SQL table for the average unemployment rate.
         unemployment_query = text(
